1a72297b1d7a965e9cd71d4ebbed9f999bcb7838
Mark <acidburn101@users.noreply.github.com>
2017-06-22 12:29:47 +0200

    Some more improvements

--- launchServer.bat
+++ launchServer.bat
@@ -8,37 +8,56 @@
 :: Echo Code for Debug - default off
 @ECHO off
 
+:: All Parameters are in local scope
+SETLOCAL
+cls
+
 :: Window Title
 TITLE Launch Arma3Server Instance
 
-:: All Parameters are in local scope
-SETLOCAL
-:: Clear Screen
-cls
-
 :: General Params
+:: Adjust below Params or use Input
 set scriptVersion=0.0.1
+set gamePort=2302
 set instanceName=Arma3 Public Server
 set "installRootDir=C:\Arma3Server"
+:: Sets executable either normal or new 64bit
+:: Default: arma3server.exe
+:: 64-bit Version: arma3server_x64.exe
+set arma3Exec=arma3server_x64.exe
 :: No spaces in serverName!
 set serverName=Public_Server
-set gamePort=2302
+
+:: Paths to Profile, Server Config, Network Config
+set pathToProfile=%installRootDir%\TADST\%serverName%
+set pathToConfig=%installRootDir%\TADST\%serverName%\TADST_config.cfg
+set pathToNetworkConfig=%installRootDir%\TADST\%serverName%\TADST_basic.cfg
 
 :: Additional Startup Params
 set "startupParams=-filePatching -netlog -hugePages -maxMem=10240 -noSound -cpuCount=4"
 
 :: Modlist (Copy paste from generated TADST Config)
-set "modList=curator;kart;mark;heli;jets;C:\Arma3ServerMods\@CBA_A3;C:\Arma3ServerMods\@ace;C:\Arma3ServerMods\@acex;C:\Arma3ServerMods\@AdvancedSlingLoading;C:\Arma3ServerMods\@bzly_no_bluf_radio;C:\Arma3ServerMods\@blastcore;C:\Arma3ServerMods\@CUP_Terrains_Core;C:\Arma3ServerMods\@task_force_radio;C:\Arma3ServerMods\@3cb_baf_equipment;C:\Arma3ServerMods\@3cb_baf_weapons;C:\Arma3ServerMods\@3cb_baf_vehicles;C:\Arma3ServerMods\@CUP_Terrains_Maps;C:\Arma3ServerMods\@TRYK;C:\Arma3ServerMods\@expbg_media;C:\Arma3ServerMods\@expbg_misc;C:\Arma3ServerMods\@expbg_tfar_labels_a3;C:\Arma3ServerMods\@expbg_equipment;C:\Arma3ServerMods\@expbg_weapons_pack;C:\Arma3ServerMods\@expbg_baf_units;C:\Arma3ServerMods\@immerse;C:\Arma3ServerMods\@immersioncigs;C:\Arma3ServerMods\@tfw_radios;C:\Arma3ServerMods\@expbg_vehicles;C:\Arma3ServerMods\@expbg_factions;C:\Arma3ServerONLYMods\@expbg_scripts"
+set "modList=curator;kart;mark;heli;jets;C:\Arma3ServerMods\@CBA_A3"
+
+:: ============================
+:: ==== DO NOT EDIT BELOW =====
+:: ============================
+
+:: Check if Server was installed
+if not exist "%installRootDir%\%arma3Exec%" ^
+GOTO :exit_script
 
 :: Go to the Arma3Server Root Directory
 :: allows to drop the batch file anywhere on the server
 cd %installRootDir%
 
+:: Observer of executable
 :observer
+cls
 echo (%time%) %instanceName% started.
 
 :: Launch Instance
-start "%instanceName%" /wait /high "arma3server_x64.exe" -port=%gamePort% "-config=%installRootDir%\TADST\%serverName%\TADST_config.cfg" "-cfg=%installRootDir%\TADST\%serverName%\TADST_basic.cfg" "-profiles=%installRootDir%\TADST\Campaign_Server" -name=%serverName% %startupParams% "-mod=%modList%"
+start "%instanceName%" /wait /high "arma3Exec" -port=%gamePort% "-config=%pathToConfig%" "-cfg=%pathToNetworkConfig%" "-profiles=%pathToProfile%" -name=%serverName% %startupParams% "-mod=%modList%"
 
 :: If server instance crashed or closed
 :: Window needs to stay open for it to work
